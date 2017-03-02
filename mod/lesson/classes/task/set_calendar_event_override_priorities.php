bd83404ca60494c80856559f6012b0ed6d6ef39a
Jun Pataleta <jun@moodle.com>
2017-03-02 14:16:46 +0800

    MDL-55956 mod_lesson: Adhoc task for setting priorities for overrides

---
+++ mod/lesson/classes/task/set_calendar_event_override_priorities.php
@@ -0,0 +1,84 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Adhoc task that handles the setting of event priority for existing mod_lesson calendar events for user and group overrides.
+ *
+ * @package    mod_lesson
+ * @copyright  2017 Jun Pataleta
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_lesson\task;
+
+require_once($CFG->dirroot . '/calendar/lib.php');
+require_once($CFG->dirroot . '/mod/lesson/lib.php');
+
+use core\task\adhoc_task;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class that handles the setting of event priority for existing mod_lesson calendar events for user and group overrides.
+ *
+ * @package     mod_lesson
+ * @copyright   2017 Jun Pataleta
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class set_calendar_event_override_priorities extends adhoc_task {
+
+    /**
+     * Run the task and set the priorities of mod_lesson user and group override calendar events.
+     */
+    public function execute() {
+        global $DB;
+
+        // Set priority of user overrides.
+        $params = [
+            'modulename' => 'lesson',
+            'courseid' => 0,
+            'groupid' => 0,
+            'repeatid' => 0
+        ];
+        $DB->set_field('event', 'priority', CALENDAR_EVENT_USER_OVERRIDE_PRIORITY, $params);
+
+        // Set priority for group overrides for existing lesson events.
+        // Fetch lessons with group overrides.
+        $sql = "SELECT DISTINCT l.id
+                           FROM {lesson} l
+                     INNER JOIN {lesson_overrides} lo
+                             ON l.id = lo.lessonid
+                                AND lo.groupid IS NOT NULL";
+        $lessonsrs = $DB->get_recordset_sql($sql);
+        foreach ($lessonsrs as $record) {
+            $grouppriorities = lesson_get_group_override_priorities($record->id);
+            foreach ($grouppriorities as $key => $priorities) {
+                foreach ($priorities as $timestamp => $priority) {
+                    $select = "modulename = :modulename AND instance = :instance AND eventtype = :eventtype
+                        AND groupid <> 0 AND timestart = :timestart AND repeatid = 0";
+                    $params = [
+                        'modulename' => 'lesson',
+                        'instance' => $record->id,
+                        'eventtype' => $key,
+                        'timestart' => $timestamp
+                    ];
+                    $DB->set_field_select('event', 'priority', $priority, $select, $params);
+                }
+            }
+        }
+        $lessonsrs->close();
+    }
+}
