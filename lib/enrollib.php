a3fb30af096a28b52c5449f446d42141399d8bda
Jun Pataleta <jun@moodle.com>
2017-07-13 10:29:17 +0800

    MDL-59369 enrol: Edit user enrolment fragment

--- lib/enrollib.php
+++ lib/enrollib.php
@@ -1415,6 +1415,32 @@ function enrol_send_welcome_email_options() {
 }
 
 /**
+ * Serve the user enrolment form as a fragment.
+ *
+ * @param array $args List of named arguments for the fragment loader.
+ * @return string
+ */
+function enrol_output_fragment_user_enrolment_form($args) {
+    global $CFG, $DB;
+
+    $args = (object) $args;
+    $context = $args->context;
+    require_capability('moodle/course:enrolreview', $context);
+
+    $ueid = $args->ueid;
+    $userenrolment = $DB->get_record('user_enrolments', ['id' => $ueid], '*', MUST_EXIST);
+    $customdata = [
+        'ue' => $userenrolment,
+        'modal' => true,
+    ];
+
+    require_once("$CFG->dirroot/enrol/editenrolment_form.php");
+    $mform = new \enrol_user_enrolment_form(null, $customdata);
+
+    return $mform->render();
+}
+
+/**
  * All enrol plugins should be based on this class,
  * this is also the main source of documentation.
  */
