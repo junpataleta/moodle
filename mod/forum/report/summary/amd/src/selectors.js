602029b1c2834138bc8c0303f81ba955de675bd8
Jun Pataleta <jun@moodle.com>
2019-10-10 15:12:54 +0800

    MDL-66076 forumreport_summary: Fix suggestions

---
+++ mod/forum/report/summary/amd/src/selectors.js
@@ -0,0 +1,12 @@
+export default {
+    filters: {
+        group: {
+            checkbox: '[data-region="filter-groups"] input[type="checkbox"]',
+            clear: '[data-region="filter-groups"] .filter-clear',
+            popover: '#filter-groups-popover',
+            save: '[data-region="filter-groups"] .filter-save',
+            selectall: '[data-region="filter-groups"] .select-all',
+            trigger: '#filter-groups-button',
+        }
+    }
+};
