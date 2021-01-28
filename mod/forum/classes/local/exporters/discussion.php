c4909cc48a56ef41b2e160e4a2d34ac7829c89be
Jun Pataleta <jun@moodle.com>
2021-01-28 21:48:31 +0800

    MDL-67959 mod_forum: Display group pictures in forum only if available
    Displaying a default group picture only leads to confusion, so better
    to not display a default group picture when the group picture is not
    set for the group.

--- mod/forum/classes/local/exporters/discussion.php
+++ mod/forum/classes/local/exporters/discussion.php
@@ -178,13 +178,10 @@ class discussion extends exporter {
                     'urls' => [],
                 ];
 
-                // If not hiding the group picture, and the group has a picture then use it. Fallback to generic group image.
+                // If not hiding the group picture, and the group has a picture then use it.
                 if (!$group->hidepicture &&
                         ($url = get_group_picture_url($group, $forum->get_course_id(), true))) {
-
                     $groupdata['urls']['picture'] = $url;
-                } else {
-                    $groupdata['urls']['picture'] = $output->image_url('g/g1')->out(false);
                 }
 
                 if ($capabilitymanager->can_view_participants($user, $discussion)) {
