47b7477758d0a808f120b4adb09c0c2b84501cef
Jun Pataleta <jun@moodle.com>
2021-03-22 23:13:10 +0800

    MDL-70821 course: Remove unused completion info from cm_format
    With the activity information output component dealing with the
    completion information of the activity, there's no need to pass
    completion info to the cm_format renderable.

--- course/classes/output/section_format/cmitem.php
+++ course/classes/output/section_format/cmitem.php
@@ -25,8 +25,9 @@
 namespace core_course\output\section_format;
 
 use core_course\course_format;
+use core_course\output\section_format;
+use renderer_base;
 use section_info;
-use completion_info;
 use renderable;
 use templatable;
 use cm_info;
@@ -78,12 +79,11 @@ class cmitem implements renderable, templatable {
      * Export this data so it can be used as the context for a mustache template.
      *
      * @param renderer_base $output typically, the renderer that's calling this function
-     * @return array data context for a mustache template
+     * @return stdClass data context for a mustache template
      */
     public function export_for_template(\renderer_base $output): stdClass {
         $format = $this->format;
         $course = $format->get_course();
-        $completioninfo = new completion_info($course);
         $mod = $this->mod;
 
         $data = new stdClass();
@@ -92,7 +92,7 @@ class cmitem implements renderable, templatable {
         // This will apply styles to the course homepage when the activity information output component is displayed.
         $hasinfo = $course->showcompletionconditions == COMPLETION_SHOW_CONDITIONS || !empty($course->showactivitydates);
 
-        $item = new $this->cmclass($format, $this->section, $completioninfo, $mod, $this->displayoptions);
+        $item = new $this->cmclass($format, $this->section, $mod, $this->displayoptions);
         return (object)[
             'id' => $mod->id,
             'module' => $mod->modname,
@@ -102,4 +102,3 @@ class cmitem implements renderable, templatable {
         ];
     }
 }
-
