9a4b67a447a76ddcd1a64b5b0bee3862a3ea972f
Peter Dias <peter@moodle.com>
2021-04-08 12:37:16 +0800

    MDL-71148 core_navigation: Provide a menu header in a system context.
    - Part of: MDL-69588

--- lang/en/moodle.php
+++ lang/en/moodle.php
@@ -1006,6 +1006,7 @@ $string['hits'] = 'Hits';
 $string['hitsoncourse'] = 'Hits on {$a->coursename} by {$a->username}';
 $string['hitsoncoursetoday'] = 'Today\'s hits on {$a->coursename} by {$a->username}';
 $string['home'] = 'Home';
+$string['homeheader'] = 'Home menu';
 $string['hour'] = 'hour';
 $string['hours'] = 'hours';
 $string['howtomakethemes'] = 'How to make new themes';
