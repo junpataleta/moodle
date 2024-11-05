1e45015924ea0c3b20a5c775638e52a23a1581b5
Jun Pataleta <jun@moodle.com>
2024-11-05 12:45:58 +0800

    MDL-83634 tool_brickfield: Downgrade image alt text check's severity
    Given that
    * There is no formally defined limit for alt text length,
    * Current versions of screen readers can read alt texts longer than
    125 characters,
    * Accessibility checkers like axe DevTools, WAVE, etc., do not raise
    errors or warnings about long image alt texts.
    
    This patch:
    - Downgrades the default severity for the `img_alt_is_too_long`
    check from `BA_TEST_SEVERE` to `BA_TEST_SUGGESTION`.
    - Updates the `checkdesc:imgaltistoolong` lang string to be more of a
    reminder/suggestion rather than an error.

--- admin/tool/brickfield/classes/local/htmlchecker/common/checks/img_alt_is_too_long.php
+++ admin/tool/brickfield/classes/local/htmlchecker/common/checks/img_alt_is_too_long.php
@@ -32,7 +32,7 @@ use tool_brickfield\local\htmlchecker\common\brickfield_accessibility_test;
 class img_alt_is_too_long extends brickfield_accessibility_test {
 
     /** @var int The default severity code for this test. */
-    public $defaultseverity = \tool_brickfield\local\htmlchecker\brickfield_accessibility::BA_TEST_SEVERE;
+    public $defaultseverity = \tool_brickfield\local\htmlchecker\brickfield_accessibility::BA_TEST_SUGGESTION;
 
     /**
      * The main check function. This is called by the parent class to actually check content.
