e8c05e6a6625a05a1a9af163e6d91e8c0cb1d2fe
Jun Pataleta <jun@moodle.com>
2026-03-03 20:49:41 +0800

    MDL-87955 mod_forum: Manage subscribers accessibility improvements
    * Set a presentation role for the layout table.
    * Add accessible labels for the existing and potential subscribers
      select boxes.
    * Add accessibility Behat coverage.

--- mod/forum/renderer.php
+++ mod/forum/renderer.php
@@ -88,7 +88,13 @@ class mod_forum_renderer extends plugin_renderer_base {
         $output .= html_writer::empty_tag('input', array('type'=>'hidden', 'name'=>'sesskey', 'value'=>sesskey()));
 
         $existingcell = new html_table_cell();
-        $existingcell->text = $existinguc->display(true);
+        $existingsubslabel = html_writer::label(
+            get_string('existingsubscribers', 'forum'),
+            'existingsubscribers',
+            false,
+            ['class' => 'sr-only']
+        );
+        $existingcell->text =  $existingsubslabel . $existinguc->display(true);
         $existingcell->attributes['class'] = 'existing';
         $actioncell = new html_table_cell();
         $actioncell->text  = html_writer::start_tag('div', array());
@@ -98,11 +104,18 @@ class mod_forum_renderer extends plugin_renderer_base {
         $actioncell->text .= html_writer::end_tag('div', array());
         $actioncell->attributes['class'] = 'actions';
         $potentialcell = new html_table_cell();
-        $potentialcell->text = $potentialuc->display(true);
+        $potentialsubslabel = html_writer::label(
+            get_string('potentialsubscribers', 'forum'),
+            'potentialsubscribers',
+            false,
+            ['class' => 'sr-only']
+        );
+        $potentialcell->text = $potentialsubslabel . $potentialuc->display(true);
         $potentialcell->attributes['class'] = 'potential';
 
         $table = new html_table();
         $table->attributes['class'] = 'subscribertable boxaligncenter';
+        $table->attributes['role'] = 'presentation';
         $table->data = array(new html_table_row(array($existingcell, $actioncell, $potentialcell)));
         $output .= html_writer::table($table);
 
